083e6504f7b648c7ee2cb571fda8596e36b5d4c6
堀川 雅章 <horikawa@bemax.jp>
2016-07-12 16:35:40 +0900

    Create Sample2.java

---
+++ src/Sample2.java
@@ -0,0 +1,11 @@
+package human;
+
+public class Sample2{
+  public static void main(String[] args){
+    Human[] humen = new Human(){};
+    
+    for(Human h: humen){
+      System.out.print("name=" + h.getName());
+    }
+  }
+}
